cb1976283c4cd88c51f80264435d2b7325271e91
Mario Ortiz <ortiz.mario22@gmail.com>
2021-04-15 23:40:12 +0200

    very fun, rather easy

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
+const Movie = require('./../models/movie.model')
+
 // Endpoints
 router.get('/', (req, res) => res.render('pages/index'))
+
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .then(allMovies => {
+            res.render('pages/movies', { allMovies })
+        })
+        .catch(err => console.log('Error al importar Movies', err))
+
+})
+
+router.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(selectedMovie => {
+            console.log(selectedMovie);
+            res.render('pages/movie-detail', selectedMovie)
+        })
+        .catch(err => console.log('Error of', err))
+})
 
 
 module.exports = router
