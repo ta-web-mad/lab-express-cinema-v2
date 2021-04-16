43d4664ec68b94a08efed3811e1155d96a1f6bf2
Guille Muñoz <guillermomunozgonzalez@MacBook-Pro-de-Guillermo.local>
2021-04-16 10:00:22 +0200

    por favorrr dime que falla

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,54 @@
 const express = require('express')
+const { render, response } = require('../app')
 const router = express.Router()
 
-// Endpoints
-router.get('/', (req, res) => res.render('pages/index'))
 
+const Movie = require('./../models/Movie.model')
+
+
+// Endpoints
+router.get('/', (req, res) =>{
+ res.render('pages/index')
+})
+
+router.get('/movies-common', (req, res)=>{
+    // res.send('estas son bebebe')
+    Movie
+    .find()
+    .then(theMovies => res.render('pages/movies-common', {theMovies}))
+    .catch(err => console.log('Ha habido un error:', err))
+    console.log(theMovies)
+})
+
+router.get('/movies-common/:movie_id', (req, res, next) => {
+
+    //res.send(req.params)
+
+    const {movie_id} = req.params.id
+
+    Movie
+    .findById(movie_id)
+    .then(response => {
+            res.send(response);
+        })
+        .catch(err => console.error(err));
+})
+    
+    
+
+
+
+
+    
+
+// router.get('/libros/detalles/:book_id', (req, res) => {
+
+//     const { book_id } = req.params
+
+//     Book
+//         .findById(book_id)
+//         .then(theBook => res.render('pages/book-detail', theBook))
+//         .catch(err => console.log('Error!', err))
+// })
 
 module.exports = router
